12d146bd57ac194ee167e421d8175131aaa683b0
林岑影 <lincenying@qq.com>
2023-09-01 23:26:29 +0800

    chore: update

--- src/shims-global.d.ts
+++ src/shims-global.d.ts
@@ -1,27 +1,35 @@
-/* eslint-disable no-unused-vars */
-
 /**
- * Null 或者 T
+ * Null 或者 Undefined 或者 T
  */
-declare type Nullable<T> = T | null
+declare type Nullable<T> = T | null | undefined
 /**
  * 非 Null 类型
  */
 declare type NonNullable<T> = T extends null | undefined ? never : T
 /**
- * Undefined 或者 T
+ * 数组<T> 或者 T
  */
-declare type UnfAble<T> = T | undefined
+declare type Arrayable<T> = T | T[]
 /**
  * 键为字符串, 值为 Any 的对象
  */
 declare type Obj = Record<string, any>
-
 /**
  * 键为字符串, 值为 T 的对象
  */
 declare type ObjT<T> = Record<string, T>
-
+/**
+ * Function
+ */
+declare type Fn<T = void> = () => T
+/**
+ * 任意函数
+ */
+declare type AnyFn = (...args: any[]) => any
+/**
+ * Promise, or maybe not
+ */
+declare type Awaitable<T> = T | PromiseLike<T>
 
 declare interface ResDataLists<T> {
     data: T[]
