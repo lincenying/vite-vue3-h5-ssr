24bf004390d28e1ea6165cc04b7180788f486a49
林岑影 <lincenying@qq.com>
2023-09-22 19:53:25 +0800

    chore: update comment

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,8 @@ import App from './App.vue'
 
 console.log(`当前环境: ${import.meta.env.VITE_APP_ENV}`)
 
-// SSR requires a fresh app instance per request, therefore we export a function
-// that creates a fresh app instance. If using Vuex, we'd also be creating a
-// fresh store here.
+// SSR 需要每个请求一个新的应用程序实例，因此我们导出一个创建新应用程序实例的函数。
+// 如果使用 Vuex/Pinia，还需要创建一个新储存。
 export function createApp() {
     const app = createSSRApp(App)
     const router = createRouter()
