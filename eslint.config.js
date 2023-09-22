ed68d7fc8de2d4800a4d6fea34c309c97219a15c
林岑影 <lincenying@qq.com>
2023-09-22 23:42:52 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 import { readFile } from 'node:fs/promises'
-import eslintConfig from '@lincy/eslint-config'
+import lincy from '@lincy/eslint-config'
 import plugin from '@unocss/eslint-plugin'
 
 const autoImport = JSON.parse(
     await readFile(new URL('./.eslintrc-auto-import.json', import.meta.url)),
 )
 
-const config = eslintConfig(
+const config = lincy(
     undefined,
     {
         plugins: {
