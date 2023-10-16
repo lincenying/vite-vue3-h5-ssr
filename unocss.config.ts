7df59fe159b24381235680b4ca9a147d67ea65ad
林岑影 <lincenying@qq.com>
2023-10-16 17:45:38 +0800

    chore: update unocss.config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,7 +1,7 @@
-import unoH5Config from '@lincy/base-config/src/uno.h5.config'
+import { h5Config } from '@lincy/base-config'
 import { fontSize } from './src/design.config'
 
-export default unoH5Config({
+export default h5Config({
     baseFontSize: fontSize,
     unti: 'rem',
 })
