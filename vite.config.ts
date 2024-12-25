1fef76e1a91e5e2bd14583a0f3766ffd388d5c19
林岑影 <lincenying@qq.com>
2024-12-25 23:27:49 +0800

    fix: 将vite-plugin-mock恢复成官方版本

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import path from 'node:path'
 import process from 'node:process'
 
 import { fileURLToPath } from 'node:url'
-import { viteMockServe } from '@lincy/vite-plugin-mock'
 import UnoCSS from 'unocss/vite'
 import { defineConfig, loadEnv } from 'vite'
 import Inspect from 'vite-plugin-inspect'
+import { viteMockServe } from 'vite-plugin-mock'
 import Progress from 'vite-plugin-progress'
 
 import Build from './vite.config.build'
